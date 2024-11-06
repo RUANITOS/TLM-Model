d832936bbb81ad73cf0c737e6fccef7eb9dba687
RUANITOS <ruanguilhermesiq@yahoo.com.br>
2024-11-06 16:17:23 -0300

    "login removido, componente ainda existente"

--- src/index.js
+++ src/index.js
@@ -4,20 +4,18 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './styles/index.css';
 import reportWebVitals from './reportWebVitals';
 import Grid from './Grid';
-import Login from './Login';
+//import Login from './Login';
 import Editor from './Editor';
 import MosaicForm from './mosaicEditor';
 const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  //const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const handleLogin = () => {
-    setIsLoggedIn(true);
-  };
-
+  //const handleLogin = () => {setIsLoggedIn(true);};
+  //element={isLoggedIn ? <Grid /> : <Login onLogin={handleLogin}
   return (
     <Router>
       <Routes>
-        <Route path="/TLM-Producao" element={isLoggedIn ? <Grid /> : <Login onLogin={handleLogin} />} />
+        <Route path="/TLM-Producao" element={<Grid />} />
         <Route path="/TLM-Producao/Editor" element={<Editor />} />
         <Route path="/TLM-Producao/Mosaiceditor" element={<MosaicForm />} />
       </Routes>
