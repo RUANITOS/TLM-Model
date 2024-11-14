57ede552b0986be531905b669d75ac59e8e795d2
RUANITOS <ruanguilhermesiq@yahoo.com.br>
2024-11-14 17:48:21 -0300

    Updates

---
+++ precache-manifest.f9d2275e1772e956e1ececbe05429761.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "260d3080a4df11e0e7109a61a7f7ced3",
+    "url": "/TLM-Producao/index.html"
+  },
+  {
+    "revision": "020e856a9d5c7090b468",
+    "url": "/TLM-Producao/static/css/main.57bc0b08.chunk.css"
+  },
+  {
+    "revision": "14ca5cac053cae4ec738",
+    "url": "/TLM-Producao/static/js/2.cde3852f.chunk.js"
+  },
+  {
+    "revision": "12adf5ee6e135646e379",
+    "url": "/TLM-Producao/static/js/3.8ba39429.chunk.js"
+  },
+  {
+    "revision": "020e856a9d5c7090b468",
+    "url": "/TLM-Producao/static/js/main.44188c84.chunk.js"
+  },
+  {
+    "revision": "5d99d23c8099c429aeab",
+    "url": "/TLM-Producao/static/js/runtime~main.cc1ae956.js"
+  }
+]);
